2f8649dcee1425f0581ec899f2fd708e4daf48c2
Aaron Wubshet <13680876+aaronwubshet@users.noreply.github.com>
2024-02-25 13:31:33 -0500

    final contact form updated to work properly

--- global.js
+++ global.js
@@ -58,3 +58,13 @@ if ("colorScheme" in localStorage){
     document.documentElement.style.setProperty("color-scheme", localStorage.colorScheme);
     select.value = localStorage.colorScheme;
 }
+
+let form = document.forms["email-form"];
+let new_url = "mailto:hello@aaronwubshet.com";
+form?.addEventListener("submit", (event) => { 
+    event.preventDefault();
+    let data = new FormData(form);
+    location.href = new_url.concat("?",'subject=',encodeURIComponent(data.get("subject")),'&','body=',encodeURIComponent(data.get("body")));
+});
+
+
