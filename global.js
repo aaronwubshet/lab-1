95412440c5be60f4296b1fe4a9aaebbf6ec58acb
Aaron Wubshet <13680876+aaronwubshet@users.noreply.github.com>
2024-02-25 11:55:39 -0500

    used js for nav and updated colors and links

--- global.js
+++ global.js
@@ -4,8 +4,32 @@ function $$ (selector, context = document) {
 	return Array.from(context.querySelectorAll(selector));
 }
 
-let navLinks = $$("nav a");
 
-let currentLink = navLinks.find(a => a.host === location.host && a.pathname === location.pathname);
+let pages = {
+	"": "Home",
+	"projects/": "Projects",
+    "contact/": "Contact",
+    "resumeCV/": "Resume/CV",
+	"https://github.com/aaronwubshet/": "GitHub"
+};
 
-currentLink?.classList.add("current");
+let nav = document.createElement("nav");
+document.body.prepend(nav);
+const ARE_WE_HOME = document.documentElement.classList.contains("home");
+
+for (let url in pages) {
+	let title = pages[url];
+    url = !ARE_WE_HOME && !url.startsWith("http") ? "../" + url : url;
+	let a = document.createElement("a");
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
+
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add("current");
+    }
+    else if (a.host != location.host) {
+        a.target = "_blank";
+    }
+};
+
